fc56a2803b7609750237681365a536e2801514ec
Slash-Tec <91566059+Slash-Tec@users.noreply.github.com>
2024-01-08 16:28:37 +0100

    Paso 6 Despliegue de categorías y subcategorías

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,20 +1,10 @@
-<style>
-    #navigation-menu{
-        height: calc(100vh - 4rem);
-    }
-
-    .navigation-link:hover .navigation-submenu{
-        display: block !important;
-    }
-</style>
-
-<header class="bg-trueGray-700 sticky top-0">
+<header class="bg-trueGray-700 sticky top-0" x-data="dropdown()">
     <div class="container-menu items-center h-16">
-        <a class="flex flex-col items-center justify-center px-4 bg-opacity-25 text-white cursor-pointer font-semibold h-full">
+        <a :class="{bg-opacity-100 text-orange-500': open}" x-on:click="show()" class="flex flex-col items-center justify-center px-4 bg-opacity-25 text-white cursor-pointer font-semibold h-full">
             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path :class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
-            <span>
+            <span class="text-sm">
                 Categorías
             </span>
         </a>
@@ -48,9 +38,9 @@
         </div>
         @livewire('dropdown-cart')
     </div>
-    <nav id="navigation-menu" class="bg-trueGray-700 bg-opacity-25 w-full absolute">
+    <nav id="navigation-menu" x-show="open" :class="{'block': open, 'hidden': !open}" class="bg-trueGray-700 bg-opacity-25 w-full absolute hidden">
         <div class="container-menu h-full">
-            <div class="grid grid-cols-4 h-full relative">
+            <div x-on:click.away="close()" class="grid grid-cols-4 h-full relative">
                 <ul class="bg-white">
                     @foreach($categories as $category)
                         <li class="navigation-link text-trueGray-500 hover:bg-orange-500 hover:text-white">
@@ -74,3 +64,24 @@
         </div>
     </nav>
 </header>
+
+<script>
+    function dropdown(){
+        return{
+            open: false,
+            show(){
+                if(this.open) {
+                    this.open = false;
+                    document.getElementsByTagName('html')[0].style.overflow = 'auto'
+                }else{
+                    this.open = true;
+                    document.getElementsByTagName('html')[0].style.overflow = 'hidden'
+                }
+            },
+            close(){
+                this.open = false;
+                document.getElementsByTagName('html')[0].style.overflow = 'auto'
+            }
+        }
+    }
+</script>
