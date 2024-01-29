cdfceb6377d9d4bfb99cb34e1e9ca77f43063ae5
Slash-Tec <91566059+Slash-Tec@users.noreply.github.com>
2024-01-29 19:44:55 +0100

    Paso 18 Crear pedidos 3ª parte

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\SearchController;
@@ -29,6 +30,7 @@ Route::get('products/{product}', [ProductsController::class, 'show'])->name('pro
 Route::get('search', SearchController::class)->name('search');
 Route::get('shopping-cart', ShoppingCart::class)->name('shopping-cart');
 Route::get('orders/create', CreateOrder::class)->middleware('auth')->name('orders.create');
+Route::get('orders/{order}/payment', [OrderController::class, 'payment'])->name('orders.payment');
 
 Route::get('/deletecart', function(){
     \Cart::destroy();
