c70e57ba9ed38455cd4689ce05bd7a933dfdcd8f
Slash-Tec <91566059+Slash-Tec@users.noreply.github.com>
2024-01-31 19:42:41 +0100

    Paso 20 Gestionar pedidos

--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,20 @@ Route::middleware([
 Route::get('/deletecart', function(){
     \Cart::destroy();
 });
+
+Route::get('prueba', function(){
+   $orders = \App\Models\Order::where('status', 1)->where('created_at','<',now()->subMinutes(10))->get();
+
+   foreach ($orders as $order){
+       $items = json_decode($order->content);
+
+       foreach ($items as $item){
+           increase($item);
+       }
+
+       $order->status = 5;
+       $order->save();
+   }
+
+   return "Completado con éxito";
+});
