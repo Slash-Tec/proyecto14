d3e6c5326aae23d3e844aa773d9bcd5c46187a20
Slash-Tec <91566059+Slash-Tec@users.noreply.github.com>
2024-02-21 16:38:12 +0100

    Semana 2 Tarea 5

--- tests/Unit/CategoriesTest.php
+++ tests/Unit/CategoriesTest.php
@@ -103,8 +103,141 @@ class CategoriesTest extends TestCase
         $response->assertSee($brand->name);
     }
 
-    /*public function it_uses_subcategory_filters()*/
-    /*public function it_uses_brand_filters()*/
-    /*public function it_uses_subcategory_and_brand_filters()*/
+    /** @test **/
+    public function it_uses_brand_filters()
+    {
+        $category = Category::create([
+            'name' => 'Consola y videojuegos',
+            'slug' => 'consola-y-videojuegos',
+            'icon' => '<i class="fas fa-gamepad"></i>',
+        ]);
+
+        $monolith = Brand::create([
+            'name' => 'Monolith Soft',
+            'slug' => 'monolith',
+            'category_id' => $category->id,
+        ]);
+
+        $square = Brand::create([
+            'name' => 'Square Enix',
+            'slug' => 'square',
+            'category_id' => $category->id,
+        ]);
+
+        $xenoblade = Product::create([
+            'name' => 'Xenoblade Chronicles 3',
+            'brand_id' => $monolith->id,
+        ]);
+
+        $hearts = Product::create([
+            'name' => 'Kingdom Hearts',
+            'brand_id' => $square->id,
+        ]);
+
+        $response = $this->get('/categories/' . $category->slug . '?brand=' . $square->slug);
+        $response->assertStatus(200);
+        $response->assertSee($hearts->name);
+        $response->assertDontSee($xenoblade->name);
+    }
+
+    /** @test **/
+    public function it_uses_subcategory_filters()
+    {
+        $category = Category::create([
+            'name' => 'Consola y videojuegos',
+            'slug' => 'consola-y-videojuegos',
+            'icon' => '<i class="fas fa-gamepad"></i>',
+        ]);
+
+        $sony = Subcategory::create([
+            'name' => 'Sony',
+            'slug' => 'sony',
+            'category_id' => $category->id,
+        ]);
+
+        $nintendo = Subcategory::create([
+            'name' => 'Nintendo',
+            'slug' => 'nintendo',
+            'category_id' => $category->id,
+        ]);
+
+        $godofwar = Product::create([
+            'name' => 'God of war',
+            'subcategory_id' => $sony->id,
+        ]);
+
+        $zelda = Product::create([
+            'name' => 'Zelda',
+            'subcategory_id' => $nintendo->id,
+        ]);
+
+        $response = $this->get('/categories/' . $category->slug . '?subcategory=' . $sony->slug);
+        $response->assertStatus(200);
+        $response->assertSee($godofwar->name);
+        $response->assertDontSee($zelda->name);
+    }
+
+    /** @test **/
+    public function it_uses_subcategory_and_brand_filters()
+    {
+        $category = Category::create([
+            'name' => 'Consola y videojuegos',
+            'slug' => 'consola-y-videojuegos',
+            'icon' => '<i class="fas fa-gamepad"></i>',
+        ]);
+
+        $sony = Subcategory::create([
+            'name' => 'Sony',
+            'slug' => 'sony',
+            'category_id' => $category->id,
+        ]);
+
+        $nintendo = Subcategory::create([
+            'name' => 'Nintendo',
+            'slug' => 'nintendo',
+            'category_id' => $category->id,
+        ]);
+
+        $monolith = Brand::create([
+            'name' => 'Monolith Soft',
+            'slug' => 'monolith',
+            'category_id' => $category->id,
+        ]);
+
+        $square = Brand::create([
+            'name' => 'Square Enix',
+            'slug' => 'square',
+            'category_id' => $category->id,
+        ]);
+
+        $godofwar = Product::create([
+            'name' => 'God of war',
+            'subcategory_id' => $sony->id,
+        ]);
+
+        $neo = Product::create([
+            'name' => 'NEO The World Ends With You',
+            'brand_id' => $square->id,
+        ]);
+
+        $xenoblade = Product::create([
+            'name' => 'Xenoblade Chronicles 3',
+            'subcategory_id' => $nintendo->id,
+            'brand_id' => $monolith->id,
+        ]);
+
+        $hearts = Product::create([
+            'name' => 'Kingdom Hearts',
+            'subcategory_id' => $sony->id,
+            'brand_id' => $square->id,
+        ]);
+
+        $response = $this->get('/categories/' . $category->slug . '?subcategory=' . $sony->slug . '?brand=' . $square->slug);
+        $response->assertStatus(200);
+        $response->assertSee($hearts->name);
+        $response->assertDontSee($neo->name);
+        $response->assertDontSee($godofwar->name);
+        $response->assertDontSee($xenoblade->name);
+    }
 }
 
