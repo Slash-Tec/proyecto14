f3c2e3d3798b1cb7e6e46931aa85b4d06d31f73c
Slash-Tec <91566059+Slash-Tec@users.noreply.github.com>
2024-02-21 17:59:08 +0100

    Semana 2 Tarea 8

--- tests/Unit/ProductsTest.php
+++ tests/Unit/ProductsTest.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Models\Category;
-use App\Models\Subcategory;
 use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -28,9 +26,46 @@ class ProductsTest extends TestCase
         $response->assertSee($product->name);
         $response->assertSee($product->description);
         $response->assertSee(number_format($product->price, 2));
+        $response->assertSee('<img src="' . asset('storage/' . $product->image) . '" alt="' . $product->name . '">');
         $response->assertSee('Stock disponible: ' . $product->stock);
         $response->assertSee('<button id="btn-add-to-cart">Agregar al carrito de compras</button>');
         $response->assertSee('<button id="btn-increase-quantity">+</button>');
         $response->assertSee('<button id="btn-decrease-quantity">-</button>');
+    }
+
+    /** @test */
+    public function it_checks_maximum_stock_product()
+    {
+        $product = Product::create([
+            'name' => 'NEO The World Ends With You',
+            'slug' => 'neo',
+            'price' => 19.99,
+            'stock' => 10,
+        ]);
+
+        $response = $this->get('/products/' . $product->slug);
+        $response->assertStatus(200);
+
+        $response->assertSee('<span class="mx-2 text-gray-700">' . $product->stock . '</span>');
+        $response->assertDontSee('<button class="btn btn-primary" id="btn-increase-quantity">+</button>');
+        $response->assertSee('<button class="btn btn-primary" id="btn-increase-quantity" disabled>+</button>');
+    }
+
+    /** @test */
+    public function it_checks_minimum_stock_product()
+    {
+        $product = Product::create([
+            'name' => 'NEO The World Ends With You',
+            'slug' => 'neo',
+            'price' => 19.99,
+            'stock' => 10,
+        ]);
+
+        $response = $this->get('/products/' . $product->slug);
+        $response->assertStatus(200);
+
+        $response->assertSee('<span class="mx-2 text-gray-700">1</span>');
+        $response->assertDontSee('<button class="btn btn-danger" id="btn-decrease-quantity">-</button>');
+        $response->assertSee('<button class="btn btn-danger" id="btn-decrease-quantity" disabled>-</button>');
     }
 }
